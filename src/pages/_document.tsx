59ca9cbf4cb1d0158b618fa271eab8f92c987541
Joffre Quintas <joffrequintasdev@hotmail.com>
2023-09-26 16:34:49 -0300

    Estrutura inicial

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import {} from 'tailwindcss'
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
       <body>
         <Main />
