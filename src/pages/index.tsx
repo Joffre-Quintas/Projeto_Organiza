610352c2f1a0c690b7bec6b4f04a4074321b0842
Joffre Quintas <joffrequintasdev@hotmail.com>
2023-09-29 20:27:42 -0300

    Ajuste no tamanho da imagem na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <h2 className="text-2xl">O Seu Controle Financeiro Pessoal.</h2>
         <p><span className="text-lime-500 hover:text-lime-300"><Link href='./register'>Registre-se gratuitamente</Link></span> e comece a transformar sua vida financeira!</p>
       </div>
-      <Image src={financasImg} alt="finance" className="rounded-3xl"/>
+      <Image src={financasImg} alt="finance" className="rounded-3xl w-1/2"/>
     </div>
   )
 }
